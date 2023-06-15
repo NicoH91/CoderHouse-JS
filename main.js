7e3ad5fd3c5eebcafc62a30add57b3bdb2b7b3f6
Nicolas Haddad <nicolasmartinhaddad@gmail.com>
2023-06-15 17:46:04 -0300

    se agrego otro ciclo while + un array con las reservaciones y metodo push

--- main.js
+++ main.js
@@ -1,62 +1,101 @@
-// isNaN intenta convertir el parámetro pasado a un número. Si el parámetro no se puede convertir, devuelve true; en caso contrario, devuelve false. 
-// Esta función es útil ya que el valor NaN no puede se probado correctamente con operadores de igualdad.
+const camping = {
+    name: "Glamping Reservation Hotel",
+    reservations: []
+};
 
-// como usarlo 
-// function milliseconds(x) {
-//     if (isNaN(x)) {
-//       return 'Not a Number!';
-//     }
-//     return x * 1000;
-//   }
-  
+alert("Welcome to Glamping Reservation Hotel");
 
-alert("welcome to Glamping Reservation hotel")
 function makeReservation() {
+    let name, reservationDate, numberOfGuests, price, numberOfNights;
 
-
-    let name;
     while (true) {
-        name = prompt("what is your name?");
-        if (!isNaN(name)) {
-            alert("Name invalid. Please write letters")
-        } else {
-            alert("Thank you for your input")
+        while (true) {
+            name = prompt("What is your name?");
+            if (!isNaN(name)) {
+                alert("Name invalid. Please write letters.");
+            } else {
+                alert("Thank you for your input.");
+                break;
+            }
+        }
+
+        while (true) {
+            reservationDate = prompt("What is your reservation date? (MM/DD/YYYY)");
+
+            const dateObj = new Date(reservationDate);
+
+            if (dateObj.toString() === "Invalid Date") {
+                alert("Invalid Date. Please input in the following format MM/DD/YYYY.");
+            } else {
+                alert("Thanks for your answer.");
+                break;
+            }
+        }
+
+        while (true) {
+            numberOfGuests = parseInt(prompt("How many guests are traveling with you?"));
+            if (isNaN(numberOfGuests)) {
+                alert("Guest count is invalid. Please enter a number of guests.");
+            } else {
+                alert("Thank you for your input.");
+                break;
+            }
+        }
+
+        while (true) {
+            numberOfNights = parseInt(prompt("How many nights are you staying?"));
+            if (isNaN(numberOfNights) || numberOfNights <= 0) {
+                alert("Invalid number of nights. Please enter a positive number.");
+            } else {
+                break;
+            }
+        }
+        const pricePerNight = 38;
+        const totalPrice = numberOfNights * pricePerNight;
+        while (true) {
+
+            alert("The price per night is 38 dollars")
+            price = prompt(" Please write yes on the following box");
+
+            if (price === "YES" || price === "Yes" || price === "yes") {
+
+                break;
+            } else {
+                alert("Apologize for the inconvenience. Returning to menu.");
+                return makeReservation();
+            }
+        }
+
+
+
+        const reservation = {
+            name: name,
+            date: reservationDate,
+            guests: numberOfGuests,
+            price: price,
+            numberOfNights: numberOfNights,
+            totalPrice: totalPrice
+        };
+
+        camping.reservations.push(reservation);
+
+        let reservationInfo = "Reservation Details:\n";
+        reservationInfo += "Name: " + name + "\n";
+        reservationInfo += "Date: " + reservationDate + "\n";
+        reservationInfo += "Number of Guests: " + numberOfGuests + "\n";
+        reservationInfo += "Number of Nights: " + numberOfNights + "\n";
+        reservationInfo += "Total Price: " + totalPrice + "\n";
+
+        reservationInfo += "Price: Confirmed\n";
+
+        console.log(reservationInfo);
+
+        const continueReservation = confirm("Do you want to make another reservation?");
+        if (!continueReservation) {
             break;
         }
-
     }
-    let reservationDate;
-    while (true) {
-        reservationDate = prompt("What is your reservation date? (MM/DD/YYYY)");
-
-
-        const dateObj = new Date(reservationDate);
-
-        // Verificar si la instancia de Date es válida
-        if (dateObj.toString() === "Invalid Date") {
-            alert("Invalid Date. please input in the following format MM/DD/YYYY.");
-        } else {
-            alert("Thanks for your answer.");
-            break; // Salir del bucle mientras la fecha sea válida
-        }
-    }
-
-    let numberOfGuests;
-    while (true) {
-        numberOfGuests = parseInt(prompt("how many guests are traveling with you?"))
-        if (isNaN(numberOfGuests)) {
-            alert("Guest is invalid. Add a number of guest traveling with you ")
-        } else {
-            alert("Thank you for your input")
-            break;
-        }
-    }
-    let reservationInfo = " Reservation Details:\n ";
-    reservationInfo += "Name " + name + "\n";
-    reservationInfo += "Date " + reservationDate + "\n";
-    reservationInfo += "Number of Guest: " + numberOfGuests + "\n";
-
-    return reservationInfo;
 }
 
-console.log(makeReservation());
+makeReservation();
+console.log(camping.reservations);
